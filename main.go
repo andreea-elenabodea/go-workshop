6e102e20353e89c2decc2ca8a3db0f67b12005d2
apana <adina.pana@ionos.com>
2023-10-26 10:14:00 +0300

    generated code and testing

--- main.go
+++ main.go
@@ -2,62 +2,158 @@ package main
 
 import (
 	"encoding/json"
-	"log/slog"
 	"net/http"
+	"strconv"
 	"workshop_demo/client"
-	"workshop_demo/model"
+	"workshop_demo/converter"
+	"workshop_demo/server"
+
+	"golang.org/x/exp/slog"
 )
 
 func main() {
 
-	http.HandleFunc("/quotas",
-		func(w http.ResponseWriter, r *http.Request) {
-			token := r.Header.Get("Authorization")
+	// http.HandleFunc("/quotas",
+	// 	func(w http.ResponseWriter, r *http.Request) {
+	// 		token := r.Header.Get("Authorization")
 
-			dbaasQuotas, err := client.DBaaSQuotas(token)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				slog.Error("Error while getting DBaaS quotas: ", err)
-				return
-			}
+	// 		dbaasQuotas, err := client.DBaaSQuotas(token)
+	// 		if err != nil {
+	// 			w.WriteHeader(http.StatusInternalServerError)
+	// 			slog.Error("Error while getting DBaaS quotas: ", err)
+	// 			return
+	// 		}convert_testrr := client.DNSQuotas(token)
+	// 		if err != nil {
+	// 			w.WriteHeader(http.StatusInternalServerError)
+	// 			slog.Error("Error while getting DNS quotas: ", err)
+	// 			return
+	// 		}
 
-			dnsResponse, err := client.DNSQuotas(token)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				slog.Error("Error while getting DNS quotas: ", err)
-				return
-			}
+	// 		serverResponse := convertModels(*dnsResponse, *dbaasQuotas)
 
-			serverResponse := convertModels(*dnsResponse, *dbaasQuotas)
+	// 		jsonBody, err := json.Marshal(serverResponse)
+	// 		if err != nil {
+	// 			w.WriteHeader(http.StatusInternalServerError)
+	// 			slog.Error("Error while marshaling response: ", err)
+	// 			return
+	// 		}
 
-			jsonBody, err := json.Marshal(serverResponse)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				slog.Error("Error while marshaling response: ", err)
-				return
-			}
+	// 		w.WriteHeader(http.StatusOK)
+	// 		w.Write(jsonBody)
+	// 	},
+	// )
 
-			w.WriteHeader(http.StatusOK)
-			w.Write(jsonBody)
-		},
-	)
+	serverImplementation := ServerImplementation{}
+	handler := server.Handler(&serverImplementation)
 
 	println("starting server")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", handler)
 }
 
-func convertModels(
-	dns client.DNSResponse,
-	dbaas client.DBaaSResponse,
-) model.ServerResponse {
-	return model.ServerResponse{
-		DNSResponse: model.Quota[model.DNSQuota]{
-			Limit: dns.QuotaLimits,
-			Usage: dns.QuotaUsage,
-		},
-		DBaaSResponse: model.Quota[model.DatabaseQuota]{
-			Limit: dbaas.QuotaLimits,
-			Usage: dbaas.QuotaUsage,
-		},
+// func convertModels(
+// 	dns client.DNSResponse,
+// 	dbaas client.DBaaSResponse,
+// ) model.ServerResponse {
+// 	return model.ServerResponse{
+// 		DNSResponse: model.Quota[model.DNSQuota]{
+// 			Limit: dns.QuotaLimits,
+// 			Usage: dns.QuotaUsage,
+// 		},
+// 		DBaaSResponse: model.Quota[model.DatabaseQuota]{
+// 			Limit: dbaas.QuotaLimits,
+// 			Usage: dbaas.QuotaUsage,
+// 		},
+// 	}
+// }
+
+// func convertModels(
+//     dns model.Quota[model.DNSQuota],
+//     dbaas model.Quota[model.DatabaseQuota],
+// ) server.Quotas {
+//     return server.Quotas{
+//         DBaaS: &struct {
+//             Limits *server.DBaaSQuota "json:\"Limits,omitempty\""
+//             Usage  *server.DBaaSQuota "json:\"Usage,omitempty\""
+//         }{
+//             Limits: &server.DBaaSQuota{
+//                 CPU:              stringToInt64Ptr(dbaas.Limit.Cpu),
+//                 Memory:           stringToInt64Ptr(dbaas.Limit.Memory),
+//                 MongoClusters:    stringToInt64Ptr(dbaas.Limit.CountMongoclustersDbaasIonosCom),
+//                 PostgresClusters: stringToInt64Ptr(dbaas.Limit.CountPostgresclustersDbaasIonosCom),
+//                 Storage:          stringToInt64Ptr(dbaas.Limit.Storage)},
+//             Usage: &server.DBaaSQuota{
+//                 CPU:              stringToInt64Ptr(dbaas.Usage.Cpu),
+//                 Memory:           stringToInt64Ptr(dbaas.Usage.Memory),
+//                 MongoClusters:    stringToInt64Ptr(dbaas.Usage.CountMongoclustersDbaasIonosCom),
+//                 PostgresClusters: stringToInt64Ptr(dbaas.Usage.CountPostgresclustersDbaasIonosCom),
+//                 Storage:          stringToInt64Ptr(dbaas.Usage.Storage)},
+//         },
+//         DNS: &struct {
+//             Limits *server.DNSQuota "json:\"Limits,omitempty\""
+//             Usage  *server.DNSQuota "json:\"Usage,omitempty\""
+//         }{
+//             Limits: &server.DNSQuota{
+//                 Records:        intToInt64Ptr(dns.Limit.Records),
+//                 Zones:          intToInt64Ptr(dns.Limit.Zones),
+//                 SecondaryZones: intToInt64Ptr(dns.Limit.SecondaryZones),
+//             },
+//             Usage: &server.DNSQuota{
+//                 Records:         intToInt64Ptr(dns.Usage.Records),
+//                 Zones:           intToInt64Ptr(dns.Usage.Zones),
+//                 SecondaryZones:  intToInt64Ptr(dns.Usage.SecondaryZones),
+//             },
+//         },
+//     }
+// }
+
+func stringToInt64Ptr(s string) *int64 {
+    integer, _ := strconv.ParseInt(s, 10, 64)
+    return &integer
+}
+
+func intToInt64Ptr(i int) *int64 {
+    integer := int64(i)
+    return &integer
+}
+
+type ServerImplementation struct {
+
+}
+
+func( s *ServerImplementation) GetHealth(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+func( s *ServerImplementation) GetQuotas(w http.ResponseWriter, r *http.Request){
+
+	
+	token := r.Header.Get("Authorization")
+
+	dbaasQuotas, err := client.DBaaSQuotas(token)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("Error while getting DBaaS quotas: ", err)
+		return
 	}
+
+	dnsResponse, err := client.DNSQuotas(token)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("Error while getting DNS quotas: ", err)
+		return
+	}
+
+	serverResponse := converter.ConvertModels(*dnsResponse, *dbaasQuotas)
+
+	jsonBody, err := json.Marshal(serverResponse)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("Error while marshaling response: ", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBody)
+
 }
--- tools/go.mod
+++ tools/go.mod
@@ -1,19 +1,14 @@
-module workshop_demo
+module something
 
 go 1.21
 
 require (
-	github.com/getkin/kin-openapi v0.118.0
-	github.com/go-chi/chi/v5 v5.0.10
-	github.com/sagikazarmark/slog-shim v0.1.0
+	github.com/deepmap/oapi-codegen v1.16.2
+	github.com/golang/mock v1.6.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-)
-
-require (
+	github.com/getkin/kin-openapi v0.118.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.5 // indirect
 	github.com/invopop/yaml v0.1.0 // indirect
@@ -21,9 +16,10 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
-	github.com/stretchr/testify v1.8.4
-	github.com/ugorji/go/codec v1.2.11 // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
